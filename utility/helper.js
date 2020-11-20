ea45778f66fadcfb9371936d5201a18d70e72b49
fadhlu <fadhlurahman.i@gmail.com>
2020-11-20 23:31:51 +0800

    add random helper

--- utility/helper.js
+++ utility/helper.js
@@ -35,4 +35,8 @@ const filterBlackWhite = filename => {
   });
 };
 
-module.exports = { sleep, downloadImage, countCoeg, filterBlackWhite };
+const random = array => {
+  return Math.floor(Math.random() * array.length);
+};
+
+module.exports = { sleep, downloadImage, countCoeg, filterBlackWhite, random };
