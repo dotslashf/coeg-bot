e53f5ee785e443a294137c1036dc379a423b0353
fadhlu <fadhlurahman.i@gmail.com>
2020-11-23 23:36:55 +0700

    add coeg cooldown

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const { countCoeg, random } = require('./utility/helper');
 const { saveDataCoeg, getDataCoeg } = require('./utility/firebase');
 
 const client = new Discord.Client();
+const cooldowns = new Discord.Collection();
 
 client.commands = new Discord.Collection();
+cooldowns.set('coeg', new Discord.Collection());
 
 const commandFiles = fs
   .readdirSync('./commands')
@@ -51,27 +53,46 @@ client.on('message', async message => {
   if (message.content.startsWith(prefix)) return;
 
   if (message.content.includes('coeg') || message.content.includes('Coeg')) {
-    const emojiList = [
-      '779254975561072650',
-      '779255532136431636',
-      '779279105358430258',
-    ];
-    const n = random(emojiList);
+    const now = Date.now();
+    const timestamps = cooldowns.get('coeg');
+    const cooldownAmount = 5 * 1000;
 
-    message.react(emojiList[n]);
-    const sender_id = message.author.id;
-    const username = message.author.username;
+    if (timestamps.has(message.author.id)) {
+      const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 
-    var counter = null;
+      if (now < expirationTime) {
+        const timeLeft = (expirationTime - now) / 1000;
+        return message.reply(
+          `percuma lu spam, gak bakal ke itung coeg :V, tunggu ${timeLeft.toFixed(
+            1
+          )} baru ngomong coeg lagi dong.`
+        );
+      }
+    } else {
+      const emojiList = [
+        '779254975561072650',
+        '779255532136431636',
+        '779279105358430258',
+      ];
+      const n = random(emojiList);
 
-    try {
-      counter = await getDataCoeg(sender_id);
-    } catch (error) {
-      counter = 0;
+      message.react(emojiList[n]);
+      const author = message.guild.member(message.author);
+
+      var counter = null;
+
+      try {
+        counter = await getDataCoeg(author.user.id);
+      } catch (error) {
+        counter = 0;
+      }
+      const coegCountResult = countCoeg(message.content.split(' '));
+
+      saveDataCoeg(author.user.id, author.nickname, counter + coegCountResult);
     }
-    const coegCountResult = countCoeg(message.content.split(' '));
 
-    saveDataCoeg(sender_id, username, counter + coegCountResult);
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
   }
 });
 
