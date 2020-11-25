28768ec49269ea7a4fa69f4c724081eb5b88ab98
fadhlu <fadhlurahman.i@gmail.com>
2020-11-26 00:35:30 +0700

    remove avatar

--- commands/tolong.js
+++ commands/tolong.js
@@ -8,13 +8,10 @@ module.exports = {
   execute(message, text) {
     const { commands } = message.client;
     const embed = new Discord.MessageEmbed();
-    const author = message.guild.member(message.author);
-    const nickname = author.nickname ? author.nickname : author.user.username;
 
     embed.setTitle(`Command list untuk Coeg-BOT`);
     embed.setColor('RANDOM');
     embed.setDescription('Tolong command');
-    embed.setAuthor(nickname, message.author.avatarURL({ format: 'png' }));
     embed.setTimestamp(Date.now());
     commands.map(command => {
       embed.addField(
