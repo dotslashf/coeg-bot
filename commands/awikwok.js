e61c3c6a7ea734e3f0986d82c6ca75890fd1bf14
fadhlu <fadhlurahman.i@gmail.com>
2020-11-26 18:17:30 +0700

    Fix merge

--- commands/awikwok.js
+++ commands/awikwok.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const baseFile = './img/awikwok/';
 const files = fs.readdirSync(baseFile);
-const { random } = require('../utility/helper');
+const { random } = require('../util/helper');
 
 module.exports = {
   name: 'awikwok',
   description: 'awikwok',
   emoji: '＼⍩⃝／',
   execute(message, text) {
-    const n = random(files);
+    const n = random(files.length);
     const selectedImage = files[n];
 
     message.reply({ files: [baseFile + selectedImage] });
